0196e2d3b7f31e5a793cb99efe5735fc74059cc1
Ayush Agrawal <ayushagrawal1811@gmail.com>
2023-11-03 18:22:46 +0530

    Added Song class

---
+++ practicals/Song.php
@@ -0,0 +1,60 @@
+<?php
+class Song {
+    // Properties
+    private $title;
+    private $artist;
+    private $genre;
+    private $tempo;
+
+    // Constructor
+    public function __construct($title, $artist, $genre, $tempo) {
+        $this->setTitle($title);
+        $this->setArtist($artist);
+        $this->setGenre($genre);
+        $this->setTempo($tempo);
+    }
+
+    // Getter and Setter for Title
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    // Getter and Setter for Artist
+    public function getArtist() {
+        return $this->artist;
+    }
+
+    public function setArtist($artist) {
+        $this->artist = $artist;
+    }
+
+    // Getter and Setter for Genre
+    public function getGenre() {
+        return $this->genre;
+    }
+
+    public function setGenre($genre) {
+        $this->genre = $genre;
+    }
+
+    // Getter and Setter for Tempo
+    public function getTempo() {
+        return $this->tempo;
+    }
+
+    public function setTempo($tempo) {
+        $this->tempo = $tempo;
+    }
+}
+
+// Usage example
+$song = new Song("Song Title", "Artist Name", "Pop", 120);
+echo "Title: " . $song->getTitle() . "\n";
+echo "Artist: " . $song->getArtist() . "\n";
+echo "Genre: " . $song->getGenre() . "\n";
+echo "Tempo: " . $song->getTempo() . "\n";
+
