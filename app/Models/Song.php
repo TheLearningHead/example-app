c7e9ccf1aae927bbff8a6e634e606ddce932fa65
Ayush Agrawal <ayushagrawal1811@gmail.com>
2023-11-25 01:05:05 +0530

    Revert "Add a static HTML page to display songs"
    This reverts commit 01445810014244ff91f6e66b5aee35122746a1ed.

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -10,21 +10,13 @@ class Song extends Model
     use HasFactory;
     protected $fillable=['title', 'artist'];
 
-    //Getter and setter function for title field
+    //Getter function for title field
     public function getTitle(){
         return $this->title;
     }
 
-    public function setTitle($title) {
-        $this->title = $title;
-    }
-
-    //Getter and setter function for artist field
+    //Getter function for artist field
     public function getArtist(){
         return $this->artist;
-    }
-
-    public function setArtist($artist) {
-        $this->artist = $artist;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -32,20 +32,19 @@ Route::get('/veggies', function () {
 });
 
 Route::get('/songs_static', function () {
-    // $song1 = new Song();
-    // $song1->setTitle("Stan");
-    // $song1->setArtist("Eminem");
+    $song1 = new Song();
+    $song1->setTitle("Stan");
+    $song1->setArtist("Eminem");
   
-    // $song2 = new Song();
-    // $song2->setTitle("Nothing Else Matters");
-    // $song2->setArtist("Metallica");
+    $song2 = new Song();
+    $song2->setTitle("Nothing Else Matters");
+    $song2->setArtist("Metallica");
   
-    // $song3 = new Song();
-    // $song3->setTitle("With You");
-    // $song3->setArtist("A P Dhillon");
+    $song3 = new Song();
+    $song3->setTitle("With You");
+    $song3->setArtist("A P Dhillon");
   
-    // return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
-    return view('songs_static');
+    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
 });
 
 Route::get('/songs', function () {
