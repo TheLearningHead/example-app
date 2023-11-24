6f8e43aaf338d39fe4fcd40ffcc2c5c5872501fe
Ayush Agrawal <ayushagrawal1811@gmail.com>
2023-11-25 01:03:36 +0530

    Revert "Add a static HTML page to display songs"
    This reverts commit 88a0f82830c8f1905585d790bb907dec8229f051.

--- routes/web.php
+++ routes/web.php
@@ -49,17 +49,5 @@ Route::get('/songs_static', function () {
 });
 
 Route::get('/songs', function () {
-    $song1 = new Song();
-    $song1->setTitle("Stan");
-    $song1->setArtist("Eminem");
-  
-    $song2 = new Song();
-    $song2->setTitle("Nothing Else Matters");
-    $song2->setArtist("Metallica");
-  
-    $song3 = new Song();
-    $song3->setTitle("With You");
-    $song3->setArtist("A P Dhillon");
-  
-    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
+    return view('songs', [ 'songs' => Song::where('artist', 'A P Dhillon')->get() ] );
 });
